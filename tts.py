1ebb91f6573134e84005c0224f488515564e0d25
Nicknamewhat <99824481+nicknamewhat@users.noreply.github.com>
2023-09-25 04:38:36 +0500

    Update tts.py

--- tts.py
+++ tts.py
@@ -5,4 +5,4 @@ import sys
 
 tts = gTTS(text='ТОЧИК БАЧА ПУТИН ПАЖАЛУСТА ПАМАГАЕТ ВСЕМ ТАДЖИК БЕДНЫЙ НУ НЕ КТО НЕ ПАМАГАЕТ НА ТАДЖИК ДЕНГИ НЕТУ ДЛЯ КУПИТЬ ПРАДУКТААААААА', lang='en')
 tts.save("song.mp3")
-file = '/storage/emulated/0/documents/pydroid3/song.mp3'
+file = 'song.mp3'
