636f52974d2680d4f8ba68e55401fe9cbc74f02d
Nicknamewhat <99824481+nicknamewhat@users.noreply.github.com>
2023-11-28 16:48:08 +0500

    Update DiscordBot.py
    Ещё больше говно кода!

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,4 +1,4 @@
-import discord # Подключаем библиотеку
+import discord
 from discord.ext import commands
 from random import randint
 import random
@@ -6,17 +6,13 @@ import string
 import calendar
 import time
 from time import sleep
-
+import asyncio
 intents = discord.Intents.default() # Подключаем "Разрешения"
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-        
 # С помощью декоратора создаём первую команду
 resultt =time.localtime()
+#ротик офф
 @bot.command()
 async def gen(ctx):
 	b = ['А', 'Б', 'В', 'Г', 'У', 'Х', 'Н', 'К', 'Л']
@@ -27,20 +23,7 @@ async def gen(ctx):
 		b3 = randint(0, 8)
 		num = randint(100, 999)
 		num2 = randint(10, 99) #генерация букв и цифр для номера
-		print('---------')
-		print(b[b1])
-		print(num)
-		print(b[b2])
-		print(b[b3])
-		print(num2)
-		print('---------')
 		kones = b[b1] + str(num) + b[b2] + b[b3] + str(num2)
-		print(kones) #вывод и комбинирование букв и цифр номера
-		wrt = kones + '\n'
-		f = open('num.txt', 'a')
-		f.write(wrt)
-		f.close #сохранение в текстовик
-		#sleep(0.2) #задержка
 		await ctx.send(kones)
 @bot.command()
 async def ghoul(ctx):
@@ -53,34 +36,9 @@ async def ghoul(ctx):
 		x = x3
 		await ctx.send(x3)
 @bot.command()
-async def test(ctx):
-     
-    def generate_password(length):
-    	characters = string.ascii_letters + string.digits
-    	password = ''.join(random.choice(characters) for _ in range(length))
-    	return password
-    	
-    	for _ in range(20):
-    		length = 50
-    		password = generate_password(length)
-    		print(password)
-    await ctx.send(password)
-@bot.command()
-async def send(ctx):
-        for sraka in range(100):
-          for channel in ctx.guild.channels:
-            	if channel.name == "основной-не":
-            		
-            		channel = bot.get_channel(channel.id)
-            		test = input('')
-            		await ctx.send(test)
-@bot.command()
-async def create(ctx):
-	guild = ctx.guild
-	for pon in range(50):
-		name = 'не' + str(randint(1,500000))
-		channel = await guild.create_voice_channel(name)
-		await channel.send('Канал Создан')
+async def send(ctx, *, text):
+            			await ctx.send(f'{text}')
+            			await ctx.message.delete()
 @bot.command()
 async def time(ctx):
 	yy = (resultt.tm_year)
@@ -90,7 +48,34 @@ async def time(ctx):
 	await ctx.send(clac)
 	await ctx.send("Сейчас: " + str(mday) + " Число") #ТАДЖИК БААЧАА
 @bot.command()
-async def lol(ctx):
-	await ctx.send(f'sus')
-	await ctx.send(file=discord.File("photo.png"))
+async def ball(ctx, *, text):
+    ballr = randint(1,3)
+    balt = (f'{ctx.message.author.mention} задал вопрос **{text}** , ответ')
+    if ballr == 1:
+    	await ctx.send(balt  + f'\n Да')
+    else:
+    	if ballr == 2:
+    		await ctx.send(f'{text} \n  Нет')
+    	else:
+    		await ctx.send(f'{text} \n Спроси позже')
+@bot.command()
+async def хелп(ctx):
+	await ctx.send('Мои команды')
+	await ctx.send('!gen')
+	await ctx.send('!send')
+	await ctx.send('!ghoul')
+	await ctx.send('!ball')
+	await ctx.send('!time')
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+    message = await ctx.send('done')
+    await message.add_reaction('<:emoji_1:1171105938250408017>')
+@bot.event
+async def on_ready():
+	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="!хелп"))
+	sleep(5)
+	await bot.change_presence(activity=discord.Game(name="это тестовый бот"))
+	sleep(5)
 bot.run('TOKEN')
